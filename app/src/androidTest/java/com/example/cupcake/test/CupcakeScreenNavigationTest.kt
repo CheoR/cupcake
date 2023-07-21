a0e872761724b7b4742655dff6526bc0949f5f3d
CheoR <cheo.roman+github@gmail.com>
2023-07-21 08:23:45 -0500

    build: set up nav host for testing

--- app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
+++ app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -1,6 +1,5 @@
 package com.example.cupcake.test
 
-import android.icu.util.Calendar
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
@@ -10,24 +9,47 @@ import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import com.example.cupcake.CupcakeApp
-import com.example.cupcake.CupcakeScreen
-import com.example.cupcake.R
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import java.text.SimpleDateFormat
-import java.util.Locale
+import org.junit.Before
+
 
 /*
-    Reference TestNavHostCont roller instance to check the navigation route of the nav host to
+    Reference TestNavHostController instance to check the navigation route of the nav host to
     make sure app navigates to the correct place
 
     lateinit - declare that property can be initialized after object declared
+
+    Every test in the Test class involves testing an aspect of navigation.
+    Each test depends on the TestNavHostController object below. Instead of manually calling navhost setup
+    e.g setupCupcakeNavHost() function for every test to set up the nav controller, using @Before
+    annotation to automatically run before every method annotated with @Test.
  */
 class CupcakeScreenNavigationTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     private lateinit var navController: TestNavHostController
+
+
+    fun setupCupcakeNavHost() {
+        composeTestRule.setContent {
+            CupcakeApp()
+        }
+    }
+
+
+    @Before
+    fun setupCupcakeNavHost() {
+        composeTestRule.setContent {
+            //  use this object later to determine the navigation state,
+            //  as the app uses the controller to navigate the various screens in app.
+            navController = TestNavHostController(LocalContext.current).apply {
+                // register controller
+                navigatorProvider.addNavigator(ComposeNavigator())
+            }
+            CupcakeApp(navController = navController)
+        }
+    }
 
 }
