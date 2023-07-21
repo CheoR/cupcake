9d314cf89696893e6c4e829ff6aabe3c4ef8b321
CheoR <cheo.roman+github@gmail.com>
2023-07-21 08:06:32 -0500

    build: setup cupdake for UI tests

--- app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
+++ app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -1,4 +1,33 @@
 package com.example.cupcake.test
 
+import android.icu.util.Calendar
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.cupcake.CupcakeApp
+import com.example.cupcake.CupcakeScreen
+import com.example.cupcake.R
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+/*
+    Reference TestNavHostCont roller instance to check the navigation route of the nav host to
+    make sure app navigates to the correct place
+
+    lateinit - declare that property can be initialized after object declared
+ */
 class CupcakeScreenNavigationTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private lateinit var navController: TestNavHostController
+
 }
