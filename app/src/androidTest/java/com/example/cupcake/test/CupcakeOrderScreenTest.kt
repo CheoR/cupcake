b3da59a15c6db6527d5fdabc6d05430f3d131de1
CheoR <cheo.roman+github@gmail.com>
2023-07-23 15:16:09 -0500

    test: Cupcake order screen

---
+++ app/src/androidTest/java/com/example/cupcake/test/CupcakeOrderScreenTest.kt
@@ -0,0 +1,46 @@
+package com.example.cupcake.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.example.cupcake.ui.SelectOptionScreen
+import org.junit.Rule
+
+class CupcakeOrderScreenTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun selectOptionScreen_verifyContent() {
+        // Given list of options
+        val flavors = listOf("Vanilla", "Chocolate", "Hazelnut", "Cookie", "Mango")
+        // And subtotal
+        val subtotal = "$100"
+
+        // When SelectOptionScreen is loaded
+        // set the Compose rule content to ensure SelectOptionScreen composable launches directly
+        // so that navigation is not required and you are then able to interact with it through
+        // test instructions
+        composeTestRule.setContent {
+            SelectOptionScreen(subtotal = subtotal, options = flavors)
+        }
+
+        // Then all the options are displayed on the screen.
+        flavors.forEach { flavor ->
+            composeTestRule.onNodeWithText(flavor).assertIsDisplayed()
+        }
+
+        // confirm subtotal is displayed correctly.
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.subtotal_price,
+                subtotal
+            )
+        ).assertIsDisplayed()
+
+        // confirm the next button is disabled
+        // note the Next button is not enabled until an item is selected
+        // This test only verifies the screen content, so the last thing to test is that the Next
+        // button is disabled.
+        composeTestRule.onNodeWithStringId(R.string.next).assertIsNotEnabled()
+    }
+}
