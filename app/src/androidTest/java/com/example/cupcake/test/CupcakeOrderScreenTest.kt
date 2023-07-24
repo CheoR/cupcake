30dd3efaaa8c7f6068b493a259e48f80f09c0840
CheoR <cheo.roman+github@gmail.com>
2023-07-24 11:29:46 -0500

    fix: import missing packages

--- app/src/androidTest/java/com/example/cupcake/test/CupcakeOrderScreenTest.kt
+++ app/src/androidTest/java/com/example/cupcake/test/CupcakeOrderScreenTest.kt
@@ -1,10 +1,14 @@
 package com.example.cupcake.test
 
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import com.example.cupcake.ui.SelectOptionScreen
 import org.junit.Rule
-
+import org.junit.Test
+import com.example.cupcake.R
 class CupcakeOrderScreenTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
